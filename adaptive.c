fb73aa1d645a8d16593ac67fac82329a27f1f4f1
kaleayse <aaysekalee@gmail.com>
2026-05-07 20:20:41 +0300

    adaptive  sort processes
    adaptive fonksiyonları yazıldı

---
+++ adaptive.c
@@ -0,0 +1,72 @@
+
+
+#include "push_swap.h"
+
+void	adaptive_sort_process(t_node **a, t_node **b)
+{
+	double	disorder;
+	int		number;
+
+	if (!a || !*a || is_sorted(*a))
+		return ;
+	number = stack_size(*a);
+	disorder = measure_disorder_process(*a);
+	if (disorder < 0.2)
+		insertion_sort(a, b);
+	else if (disorder < 0.5)
+		chunk_based_sort(a, b, find_root(number));
+	else
+		radix_sort(a, b);
+}
+
+int	find_root(int total_len)
+{
+	int	root;
+
+	if (total_len <= 0)
+		return (0);
+	root = 1;
+	while (root * root <= total_len)
+	{
+		if (root * root == total_len)
+			return (root);
+		root++;
+	}
+	return (root - 1);
+}
+
+static int	count_inversions(t_node *stack)
+{
+	int		inversions;
+	t_node	*i;
+	t_node	*j;
+
+	inversions = 0;
+	i = stack;
+	while (i)
+	{
+		j = i->next;
+		while (j)
+		{
+			if (i->index > j->index)
+				inversions++;
+			j = j->next;
+		}
+		i = i->next;
+	}
+	return (inversions);
+}
+
+double	measure_disorder_process(t_node *stack)
+{
+	int	inversions;
+	int	number;
+
+	if (!stack)
+		return (0.0);
+	number = stack_size(stack);
+	if (number < 2)
+		return (0.0);
+	inversions = count_inversions(stack);
+	return ((double)inversions / ((double)number * (number - 1) / 2.0));
+}
